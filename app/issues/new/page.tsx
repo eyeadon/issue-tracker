8aa30c1c0e99b5e703cbecdae3301df2884325e2
eyeadon <eyeadon40@gmail.com>
2025-09-06 18:55:21 -0400

    organizing imports

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,20 @@
 "use client";
-import { Button, Callout, Text, TextField } from "@radix-ui/themes";
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
-import { useForm, Controller } from "react-hook-form";
-import axios from "axios";
+import { createIssueSchema } from "@/app/api/validationSchemas";
+import ErrorMessage from "@/app/components/ErrorMessage";
+import Spinner from "@/app/components/Spinner";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { z } from "zod";
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
   loading: () => <p>Loading...</p>,
 });
-import "easymde/dist/easymde.min.css";
-import { useRouter } from "next/navigation";
-import { createIssueSchema } from "@/app/api/validationSchemas";
-import { z } from "zod";
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
