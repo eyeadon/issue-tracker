c7ac307bf3f5eae211f8d1d0f5910c2d9759e9ca
eyeadon <eyeadon40@gmail.com>
2025-09-18 17:57:53 -0400

    filter issues

--- app/issues/(list)/page.tsx
+++ app/issues/(list)/page.tsx
@@ -2,9 +2,23 @@ import prisma from "@/lib/prisma";
 import { Table } from "@radix-ui/themes";
 import { IssueStatusBadge, Link } from "@/app/components";
 import IssueActions from "./IssueActions";
+import { Status } from "@/app/generated/prisma";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+interface Props {
+  searchParams: Promise<{ status: Status }>;
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+  const { status } = await searchParams;
+  const statuses = Object.values(Status);
+
+  const statusToFilter = statuses.includes(status) ? status : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status: statusToFilter },
+  });
+
+  console.log(status);
 
   return (
     <div>
