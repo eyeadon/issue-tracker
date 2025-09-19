38b8b8b930793f443f639f527b798e4d58a5a497
eyeadon <eyeadon40@gmail.com>
2025-09-19 15:12:48 -0400

    pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>Hello</div>;
-}
+import Pagination from "./components/Pagination";
+
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ page: string }>;
+}) => {
+  const { page } = await searchParams;
+
+  return (
+    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(page)} />
+  );
+};
+
+export default Home;
