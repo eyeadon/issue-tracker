44369381d8305f859a42d5786cafcb6fc108bec7
eyeadon <eyeadon40@gmail.com>
2025-09-19 12:54:13 -0400

    sort issues asc; fixing bugs

--- app/issues/(list)/page.tsx
+++ app/issues/(list)/page.tsx
@@ -17,15 +17,22 @@ const IssuesPage = async ({ searchParams }: Props) => {
 
   const statusToFilter = statuses.includes(status) ? status : undefined;
 
-  const issues = await prisma.issue.findMany({
-    where: { status: statusToFilter },
-  });
-
   const columns: { label: string; value: keyof Issue; className?: string }[] = [
     { label: "Issue", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParamsData.orderBy)
+    ? { [searchParamsData.orderBy]: "asc" }
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status: statusToFilter },
+    orderBy,
+  });
 
   return (
     <div>
@@ -34,7 +41,10 @@ const IssuesPage = async ({ searchParams }: Props) => {
         <Table.Header>
           <Table.Row>
             {columns.map((column) => (
-              <Table.ColumnHeaderCell key={column.value}>
+              <Table.ColumnHeaderCell
+                key={column.value}
+                className={column.className}
+              >
                 <NextLink
                   href={{
                     query: { ...searchParamsData, orderBy: column.value },
